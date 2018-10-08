aba671985aa18dd7984180435327adf346f3e39f
unknown <aragot.oriana.perso@gmail.com>
2018-10-08 13:00:14 +0400

    database connected

---
+++ playground/dbConnect.js
@@ -0,0 +1,31 @@
+
+import { MongoClient } from 'mongodb';
+
+MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, client) => {
+    if (err) {
+        return console.log(err);
+    }
+    console.log('connected');
+    const db = client.db('TodoApp');
+
+    db.collection('Todos').insertOne({
+        text: 'something to do',
+        completed: true
+    }, (err, result) => {
+        if (err) {
+            return console.log('unable to insert todo', err);
+        }
+        console.log(JSON.stringify(result.ops));
+    });
+
+    db.collection('Users').insertOne({
+        username: 'bob',
+        password: 'toto'
+    }, (err, result) => {
+        if (err) {
+            return console.log('unable to insert user', err);
+        }
+        console.log(JSON.stringify(result.ops));
+    });
+    client.close();
+});
